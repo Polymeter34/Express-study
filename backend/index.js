90cfdd2466efeb02471eec975fe3bab12b36eb32
Arthur Limberg <k5f48fj48@gmail.com>
2021-06-20 17:45:44 +0300

    feat: node and express setup, static pages

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,20 @@
-// import express from "express"
+import express from 'express'
+import path from 'path'
+import {requestTime, logger} from './middlewares.js'
 
-console.log("Hello from index.js");
+const __dirname = path.resolve()
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
+
+app.get('/download', (req, res) => {
+    res.download(path.resolve(__dirname, 'static', 'index.html'))
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on ${PORT}...`);
+})
